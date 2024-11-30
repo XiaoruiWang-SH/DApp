b2455c69a972366424040550fe089e5528824629
Xiaorui Wang <105865593+XiaoruiWang-SH@users.noreply.github.com>
2024-11-30 20:52:33 +0100

    Homepage & itemDes page

--- dapp/frontend/src/components/Dapp.js
+++ dapp/frontend/src/components/Dapp.js
@@ -1,128 +1,21 @@
 import React from 'react';
-import "./HomepageStyle.css";
-import GoodsItem from './GoodsItem';
-import { useState, useEffect } from 'react';
-import art from '../res/art.jpg';
-import jewellery from '../res/jewellery.jpg';
-import motorbike from '../res/motorbike.jpg';
-import watch from '../res/watch.jpg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import ItemDes from './ItemDes';
+import Layout from './Layout';
+
+
 
 export default function Dapp() {
-
-const [goodsItems, setGoodsItems] = useState([]);
-
-useEffect(() => {
-    const goodsItems = [
-        {
-          title: "Beautiful Sunset",
-          image: art,
-          description: "Enjoy the beautiful sunset from the beach."
-        },
-        {
-          title: "Mountain Adventure",
-          image: jewellery,
-          description: "Explore the majestic mountains with us."
-        },
-        {
-          title: "City Lights",
-          image: motorbike,
-          description: "Experience the vibrant city nightlife."
-        },
-        {
-          title: "hahah Lights",
-          image: watch,
-          description: "Experience the vibrant city nightlife."
-        },
-        {
-            title: "Beautiful Sunset",
-            image: art,
-            description: "Enjoy the beautiful sunset from the beach."
-          },
-          {
-            title: "Mountain Adventure",
-            image: jewellery,
-            description: "Explore the majestic mountains with us."
-          },
-          {
-            title: "City Lights",
-            image: motorbike,
-            description: "Experience the vibrant city nightlife."
-          },
-          {
-            title: "hahah Lights",
-            image: watch,
-            description: "Experience the vibrant city nightlife."
-          },
-          {
-            title: "Beautiful Sunset",
-            image: art,
-            description: "Enjoy the beautiful sunset from the beach."
-          },
-          {
-            title: "Mountain Adventure",
-            image: jewellery,
-            description: "Explore the majestic mountains with us."
-          },
-          {
-            title: "City Lights",
-            image: motorbike,
-            description: "Experience the vibrant city nightlife."
-          },
-          {
-            title: "hahah Lights",
-            image: watch,
-            description: "Experience the vibrant city nightlife."
-          }
-      ];
-      setGoodsItems(goodsItems);
-  }, [setGoodsItems]);
-
-
-const Header = () => {
     return (
-      <header >
-        <h1>{"Auction System"}</h1>
-      </header>
-    );
-  };  
-  
-  const Content = () => {
-    return (
-      <main>
-        <div className='card-container'>
-            {
-                goodsItems.map((goodsItem, index) => (
-                    <GoodsItem
-                        key={index}
-                        title={goodsItem.title}
-                        image={goodsItem.image}
-                        description={goodsItem.description}
-                    />
-                ))
-            }
-        </div>
-      </main>
-    );
-  };
-  
-  
-  const Footer = () => {
-    return (
-      <footer >
-        <p>© 2024 My Website</p>
-      </footer>
-    );
-  };
-
-  return (
-    <div className='container'>
-      <Header />
-      <div className='main-section'>
-        {/* <Sidebar /> */}
-        <Content />
-      </div>
-      <Footer />
-    </div>
-  );
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />} >
+                <Route index element={<HomePage />} />
+                <Route path="itemDes" element={<ItemDes />} />
+            </Route>
+          </Routes>
+        </Router>
+      );
 
 };
--- dapp/frontend/src/components/GoodsItem.js
+++ dapp/frontend/src/components/GoodsItem.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './GoodsItemStyle.css';
 
-export default function GoodsItem({ title, image, description }) {
+export default function GoodsItem({ title, image, startBid, highestBid, totalBids, endTime, onClick }) {
+    // console.log("highestBid:" + highestBid);
+    // console.log("totalBids:" + totoalBids);
   return (
-    <div className="card">
+    <div className="card" onClick={onClick}>
       <img src={image} alt={title} className="card-image" />
+    
       <div className="card-content">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
+        <p className="card-title">{title}</p>
+        <hr />
+        <div className="card-bid">
+            <text className='left-text'>Start Bid: </text>
+            <text className='right-text'>{startBid}</text>
+        </div>
+        <div className="card-bid">
+            <text className='left-text'>Highest Bid: </text>
+            <text className='right-text'>{highestBid}</text>
+        </div>
+        <div className="card-bid">
+            <text className='left-text'>Total Bids: </text>
+            <text className='right-text'>{totalBids}</text>
+        </div>
+        <div className="card-bid">
+            <text className='left-text'>End Time: </text>
+            <text className='right-text'>{endTime}</text>
+        </div>
+
+        
       </div>
     </div>
   );
